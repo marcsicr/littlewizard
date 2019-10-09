752f6fb117bc53cfbc2c869178a57ec1ef0c9a7c
marcsicr <marcsicr@uoc.edu>
2019-10-09 15:18:48 +0200

    Basic scene with player movement

--- littlewizard/Assets/Scripts/Camera/CameraPlayer.cs
+++ littlewizard/Assets/Scripts/Camera/CameraPlayer.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CameraPlayer : MonoBehaviour
 {
+    public GameObject target;
+    private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
+        target = GameObject.FindGameObjectWithTag("Player");
         
+
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 newPos = new Vector3(
+            target.transform.position.x, 
+            target.transform.position.y, 
+            transform.position.z);
+
+        transform.position = newPos;
     }
 }
---
+++ littlewizard/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 2f;
+    private Vector2 change;
+    private Rigidbody2D myRigidBody;
+    private Animator myAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        
+        //Set player initial orientation
+        myAnimator.SetFloat("moveX", 0.0f);
+        myAnimator.SetFloat("moveY", -1.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector2.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+      
+
+        if (change != Vector2.zero)
+        {
+            change.Normalize();
+            myRigidBody.MovePosition((Vector2)transform.position + change * speed * Time.deltaTime);
+            myAnimator.SetBool("walking", true);
+            myAnimator.SetFloat("moveX", change.x);
+            myAnimator.SetFloat("moveY", change.y);
+        }
+        else
+        {
+            myAnimator.SetBool("walking", false);
+        }
+
+        
+
+    }
+}
