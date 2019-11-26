ef6fb9139b3cb19c1867aff387a27813e4cf72ed
marcsicr <marcsicr@uoc.edu>
2019-11-26 10:54:33 +0100

    Bridges & Spells Levels

---
+++ littlewizard/Assets/Scripts/Items/SpellBook.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellBook : Item
+{
+    public ObservableInt levelBolt;
+
+    public override void onItemCollect(Player player) {
+
+        levelBolt.UpdateValue(levelBolt.getRunTimeValue() + 1);
+        Debug.Log("Book collected");
+    }
+}
---
+++ littlewizard/Assets/Scripts/MechanismsComponents/Bridge.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : Activable
+{
+    Animator myAnimator;
+
+    protected override void Start() {
+
+        base.Start();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    public override void Activate() {
+        myAnimator.SetBool("active", true);
+    }
+
+    public override void Desactivate() {
+        myAnimator.SetBool("active", false);
+    }
+}
